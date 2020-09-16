2f7efeeed22c1bc32cc7ff68eda7fa9bd41124e2
Artem Nikolaev <artem@nikolaev.by>
2020-09-17 00:45:39 +0300

    3.1 прикручиваем базу к заказу заказов

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,9 +1,29 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+
 function createOrder(order) {
-	if (!order || !order.pizzaId || !order.address) {
+	if (!order || !order.pizza || !order.address) {
 		throw new Error('To order pizza please provide pizza type and address where pizza should be delivered');
 	}
 
-	return {};
+	return docClient.put({
+		TableName: 'pizza-orders',
+		Item: {
+			orderId: uuid(),
+			pizza: order.pizza,
+			address: order.address,
+			orderStatus: 'pending',
+		}
+	}).promise()
+		.then(res => {
+			console.log('Order is saved!', res);
+			return res;
+		})
+		.catch(err => {
+			console.log('Oops, order is not saved :(', err);
+			throw err;
+		});
 }
 
 module.exports = createOrder;
